af4ddcb80425d6e7478216833e31b7d4dd09b32a
Alan Almeida <alanalmeida.emp@gmail.com>
2024-10-25 18:36:08 -0300

    feat: implement radio buttons

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -6,11 +6,28 @@ import { TextInput } from '../../components/TextInput';
 import { LargeButton } from '../../components/Button';
 import { AuthStackParamsList } from '../../routes/AuthStack';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RadioGroup } from 'react-native-radio-buttons-group';
+
+const radioButtons = [
+  {
+      id: '1',
+      label: 'Empresa',
+      value: 'option1',
+  },
+  {
+      id: '2',
+      label: 'Colaborador',
+      value: 'option2',
+  },
+];
+
 
 export function LoginScreen({navigation}: NativeStackScreenProps<AuthStackParamsList, 'LoginScreen'>) {
 
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [selectedId, setSelectedId] = React.useState('1');
+
 
   function goToSignUpPage() {
     navigation.navigate('SignUpScreen');
@@ -21,8 +38,22 @@ export function LoginScreen({navigation}: NativeStackScreenProps<AuthStackParams
       <Text style={styles.appNameText} preset="headingLarge" bold>
         Ponto Fácil - Login
       </Text>
-      <TextInput onChangeText={setEmail} value={email} placeholder="E-mail" keyboardType="email-address"/>
-      <TextInput onChangeText={setPassword} value={password} placeholder="Senha"/>
+      <TextInput onChangeText={setEmail} value={email} placeholder="E-mail" keyboardType="email-address" autoCapitalize="none" />
+      <TextInput onChangeText={setPassword} value={password} placeholder="Senha" />
+      <RadioGroup
+            radioButtons={radioButtons}
+            onPress={(id) => setSelectedId(id)}
+            selectedId={selectedId}
+            labelStyle={{
+              color: '#000030',
+            }}
+            containerStyle={{
+              display: 'flex',
+              width: '100%',
+              alignItems: 'flex-start',
+            }}
+            layout="row"
+        />
       <LargeButton title="Entrar" containerStyle={{ marginTop: 8 }} />
       <LargeButton variant="text" title="Criar conta" containerStyle={{ marginTop: 8}} onPress={goToSignUpPage} />
     </Screen>
--- src/screens/SignUpScreen/index.tsx
+++ src/screens/SignUpScreen/index.tsx
@@ -6,27 +6,60 @@ import { TextInput } from '../../components/TextInput';
 import { LargeButton } from '../../components/Button';
 import { AuthStackParamsList } from '../../routes/AuthStack';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import RadioGroup from 'react-native-radio-buttons-group';
 
 export function SignUpScreen({navigation}: NativeStackScreenProps<AuthStackParamsList, 'SignUpScreen'>) {
 
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [confirmPassword, setConfirmPassword] = React.useState('');
+  const [selectedId, setSelectedId] = React.useState('1');
+  const [companyId, setCompanyId] = React.useState('');
 
   function goToSignUpPage() {
     navigation.goBack();
   }
+
+  const radioButtons = React.useMemo(() => ([
+    {
+        id: '1',
+        label: 'Empresa',
+        value: 'option1',
+    },
+    {
+        id: '2',
+        label: 'Colaborador',
+        value: 'option2',
+    },
+]), []);
+
 
   return (
     <Screen style={styles.container}>
       <Text style={styles.appNameText} preset="headingLarge" bold>
         Ponto Fácil - Cadastro
       </Text>
-      <TextInput onChangeText={setEmail} value={email} placeholder="E-mail"/>
-      <TextInput onChangeText={setPassword} value={password} placeholder="Senha" secureTextEntry/>
+      <TextInput onChangeText={setEmail} value={email} placeholder="E-mail" autoCapitalize="none"/>
+      <TextInput onChangeText={setPassword} value={password} placeholder="Senha" secureTextEntry autoCapitalize="none"/>
       <TextInput onChangeText={setConfirmPassword} value={confirmPassword} placeholder="Confirmar senha" secureTextEntry />
+      { selectedId === '2' && <TextInput onChangeText={setCompanyId} value={companyId} placeholder="Código da empresa" secureTextEntry />}
+      <RadioGroup
+            radioButtons={radioButtons}
+            onPress={(id) => setSelectedId(id)}
+            selectedId={selectedId}
+            labelStyle={{
+              color: '#000030',
+            }}
+            containerStyle={{
+              display: 'flex',
+              width: '100%',
+              alignItems: 'flex-start',
+            }}
+            layout="row"
+        />
       <LargeButton title="Criar conta" containerStyle={{ marginTop: 8 }} />
       <LargeButton variant="text" title="Voltar" containerStyle={{ marginTop: 8}} onPress={goToSignUpPage} />
     </Screen>
   );
 }
+
