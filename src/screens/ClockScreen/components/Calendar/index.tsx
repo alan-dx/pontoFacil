0b84c242941e310bf59da6ac079fa16fb94dda48
Alan Almeida <alanalmeida.emp@gmail.com>
2024-11-29 21:57:04 -0300

    feat: implement calendar

--- src/screens/ClockScreen/components/Calendar/index.tsx
+++ src/screens/ClockScreen/components/Calendar/index.tsx
@@ -1,18 +1,43 @@
 import React from 'react';
 
-import { TouchableOpacity, View } from 'react-native';
+import { Modal, TouchableOpacity, View } from 'react-native';
 import { Text } from '../../../../components/Text/Text';
 import Icon from 'react-native-vector-icons/Feather';
 import { styles } from './styles';
 import { addDays, format } from 'date-fns';
+import { Calendar as RNCalendar } from 'react-native-calendars';
+import {LocaleConfig} from 'react-native-calendars';
+import { theme } from './theme';
 
+LocaleConfig.locales.ptBr = {
+  monthNames: [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
+  ],
+  monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul.', 'Ago', 'Set', 'Otu', 'Nov', 'Dez'],
+  dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+  dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+  today: 'Hoje',
+};
+
+LocaleConfig.defaultLocale = 'ptBr';
 interface CalendarProps {
   onChangeDate: (date: Date) => void;
   currentDate: Date
 }
 
 export function Calendar({onChangeDate, currentDate}: CalendarProps) {
-
+  const [modalVisible, setModalVisible] = React.useState(false);
 
   const onPressRight = () => {
       const newDate = addDays(currentDate, 1);
@@ -29,18 +54,45 @@ export function Calendar({onChangeDate, currentDate}: CalendarProps) {
       <TouchableOpacity style={styles.dayButtonBox} onPress={onPressLeft}>
         <Icon name="chevron-left" size={20} color="#000" />
       </TouchableOpacity>
-      <TouchableOpacity style={styles.currentDateBox}>
+      <TouchableOpacity style={styles.currentDateBox} onPress={() => setModalVisible(!modalVisible)}>
         <Text style={styles.currentDateBoxText}>{format(currentDate, 'dd/MM/yyyy')}</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.dayButtonBox} onPress={onPressRight}>
         <Icon name="chevron-right" size={20} color="#000" />
       </TouchableOpacity>
-      {/* <RNCalendar
-      onDayPress={day => {
-        onChangeDate(new Date(day.timestamp));
-      }}
 
-    /> */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onDismiss={() => {
+          setModalVisible(!modalVisible);
+        }}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <RNCalendar
+            markedDates={{
+              [format(currentDate, 'yyyy-MM-dd')]: {selected: true, selectedColor: '#0048ce'},
+            }}
+            onDayPress={(day: any) => {
+              onChangeDate(new Date(day.timestamp));
+              setModalVisible(false);
+            }}
+            theme={theme}
+            renderHeader={(date: string) => {
+              return (
+                <View>
+                  <Text>{format(date, 'dd/MM/yyyy')}</Text>
+                </View>
+              );
+            }}
+          />
+        </View>
+        </View>
+      </Modal>
     </View>
  );
 }
--- src/screens/ClockScreen/components/Calendar/styles.ts
+++ src/screens/ClockScreen/components/Calendar/styles.ts
@@ -27,4 +27,41 @@ export const styles = StyleSheet.create({
     fontSize: 14,
     color: '#000',
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonClose: {
+    backgroundColor: '#0048ce',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
 });
